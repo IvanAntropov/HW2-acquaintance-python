89a34627d8d4322a2f902df1d7284988a91a5963
Ivan <vana.antropov@gmail.com>
2022-07-13 23:14:51 +0500

    Did ex4

--- Ex4.py
+++ Ex4.py
@@ -1,6 +1,6 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 
-def InputValues (text: str):
+def InputValues(text: str):
     check = False
     while not check:
         try:
@@ -10,20 +10,38 @@ def InputValues (text: str):
             number = int(input(f'{text}'))
     return number
 
+
 def makeTheList(num):
     helpN = -num
     listOfNumbers = []
     while helpN != num + 1:
         listOfNumbers.append(helpN)
-        helpN+=1
-    return listOfNumbers 
+        helpN += 1
+    return listOfNumbers
 
-# def multiOfElements(listN):
- 
 
+def makeTextPositionAndMulti(listOfNumbers):
+    
+    position = input('Enter position: ') # я бы мог сделать проверку на дурака, но я много времени потратил чтоб разобрать с файлами
+    position = position.strip()
+    while "  " in position:
+        position= position.replace("  ", " ")
+    positionInList = position.split(' ')
+    
+    with open('file.txt', 'w') as data:
+        data.write(f'{positionInList[0]}\n')
+        data.write(f'{positionInList[1]}')
+        data.close()
+    with open("file.txt", "r") as file:
+        firsPosition =  file.readline()
+        secondPosition = file.readline()
+    multiOfPosition = listOfNumbers[int(firsPosition)] * listOfNumbers[int(secondPosition)]
+    print(multiOfPosition)
+
+    
 numberN = InputValues('Enter number N: ')
-position = input('Enter position: ')
-position.split(' ')
-print(type(position))
-print (makeTheList(numberN))
-# print(multiOfElements(makeTheList(numberN)))
+listOfNumbers = makeTheList(numberN)
+print(listOfNumbers)
+makeTextPositionAndMulti(listOfNumbers)
+
+
--- Ex5.py
+++ Ex5.py
@@ -1,1 +1,30 @@
 # Реализуйте алгоритм перемешивания списка.
+
+def InputValuesList (text: str):
+    check = False
+    listForNumbers = []
+    while not check:
+        try:
+            number = int(input(f'{text}'))
+            listForNumbers.append(number)
+            if len(listForNumbers) == 5:
+                check = True
+        except ValueError:
+            number = int(input(f'{text}'))
+    return listForNumbers
+
+def MixList(listOfNumber: list):
+    
+    for i in range(len(listOfNumber)):
+        helpN = listOfNumber[i]
+        import random
+        randomN = random.randint(i,4)
+        while randomN == i:
+            randomN = random.randint(0,4)
+        listOfNumber[i] = listOfNumber[randomN]
+        listOfNumber[randomN] = helpN
+    return listOfNumber
+
+listOfNumber = InputValuesList ("Enter numbers for list: ")
+print(f'Origin list: {listOfNumber}')
+print(f'Mixed list: {MixList(listOfNumber)}')
